446a1a2f9f552df9463372ba25453b9ced922023
Michael Mano <michael.mano26@gmail.com>
2017-10-23 17:08:28 +1000

    Adds cropperjs - will need refactoring

--- resources/assets/js/vanilla/index.js
+++ resources/assets/js/vanilla/index.js
@@ -2,12 +2,14 @@ import Q from 'qoob';
 import pre from './components/pre';
 import form from './components/form';
 import sortable from './components/sortable';
+import crop from './components/crop';
 
 const module = function module() {
   Q.documentReady(() => {
     form();
     pre();
     sortable();
+    crop();
   });  
 };
 
