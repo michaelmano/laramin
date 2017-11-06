3e816a85cba9b26789551ab9372545ad728c41b7
Michael Mano <michael.mano26@gmail.com>
2017-11-06 10:04:13 +1000

    Adds a simple loader to be used where a full page overlay is not required.

---
+++ resources/assets/js/vue/components/SimpleLoader.vue
@@ -0,0 +1,23 @@
+<template>
+		<div v-show="loading" class="Loader__icon">
+			<div class="Loader__square Loader__square1"></div>
+			<div class="Loader__square Loader__square2"></div>
+			<div class="Loader__square Loader__square3"></div>
+			<div class="Loader__square Loader__square4"></div>
+			<div class="Loader__square Loader__square5"></div>
+			<div class="Loader__square Loader__square6"></div>
+			<div class="Loader__square Loader__square7"></div>
+			<div class="Loader__square Loader__square8"></div>
+			<div class="Loader__square Loader__square9"></div>
+		</div>
+</template>
+<script>
+	export default {
+		props: {
+			loading: {
+				default: false,
+				type: Boolean
+			}
+		}
+	}
+</script>
